ae265c8f08f1471c31652dd48cb70b301c40a912
praveen230701244 <230701244@rajalakshmi.edu.in>
2026-04-08 20:12:57 +0530

    t1

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_URL || "https://skill-gap-backend-goi5.onrender.com";
 
 console.log("API BASE:", API_BASE_URL);
 
